1253a3e8ab4e0e248ab6c88a916ebb006515253e
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2023-04-06 10:46:22 +0530

    added the navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,3 @@
+<nav class="h-[10vh] bg-black">
+    <h1 class="text-white text-center text-2xl font-bold">LOGO</h1>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Note1 from "../components/note1.svelte";
+    import Navbar from "../components/navbar.svelte";
 </script>
 
 
 <section class="">
     <div class="">
+        <Navbar/>
         <Note1/>
     </div>
 </section>
