e114e3a093957977aaba8c0cc6061ecf68dde3b1
rajesh6303237219 <rajeshbogadi123@gmail.com>
2023-04-21 10:32:49 +0530

    about

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -1,4 +1,4 @@
-<section class="h-[90vh]">
+<section id="main" class="h-[90vh]">
     <h1 id="about" class="text-center">ABOUT</h1>
 <div id=container>
 <div id=kkplogo>
@@ -30,7 +30,7 @@ an experiencd investor , we are help to help you navigate the n market and achei
    height: 0vh;
    }
  #kkplogo img{
-    height:50vh;
+    width: 30vw;
    }
    #container1 p{
     display: flex;
@@ -50,84 +50,271 @@ an experiencd investor , we are help to help you navigate the n market and achei
        margin-top:8%;
        gap:4%;
    }
-  @media screen and (max-width:1061px){
-    #kkplogo img{
-        height:45vh;
+   @media screen and (max-width:2561px){
+    #about{
+         font-size:135px;
+         margin-top:50px;
     }
     #container{
-         margin-top:130px;
+    margin-top:10%;
     }
-  }
-  @media screen and (max-width:931px){
+    #container1 p{
+           font-size:29px;
+           line-height:53px;
+           margin-top:69px;
+    }
+   }
+   @media screen and (max-width:2187px){
+    #container{
+       display: flex;
+       justify-content: center;
+       margin-top:14%;
+    }
+   }
+   @media screen and (max-width:2017px){
     #kkplogo img{
-         height:43vh ;
+       height:45vh;
+       margin-top:3%;
     }
-  }
-  @media screen and (max-width:886px){
+   }
+   @media screen and (max-width:1954px){
+    #container1 p{
+       width:45vw;
+    }
+    #container{
+        gap:3px;
+    }
+   }
+   @media screen and (max-width:1910px){
+    #kkplogo img{
+         height:40vh;
+    }
+   }
+   @media screen and (max-width:1441px){
+    #container1 p{
+       font-size:17px;
+       line-height:32px;
+       width:39vw;
+       font-weight: bold;
+    }
+    #about{
+      font-size:60px;
+    }
+    #container{
+     margin-top: 90px;
+     gap:110px;
+    }
+    #kkplogo img{
+      height:40vh;
+      margin-top:56px;
+    }
+   }
+   @media screen and (max-width:1283px){
+    #container1 p{
+        width:42vw;
+   }
+   #container{
+      gap:8%;
+      margin-top:8%;
+   }
+}
+#container1 p{
+   width:40vw;
+   font-size:15px;
+} 
+@media screen and (max-width:1024px){
+    #container1 p{
+        font-size:15px;
+        width:40vw;
+    }
+}
+@media screen and (max-width:986px){
+    #container1 p{
+       font-size:15px;
+       width:40vw;
+    }
+    #container{
+       gap:12px;
+    }
     #kkplogo img{
        height:40vh;
     }
-    #container1 p{
-          font-size:13.6px;
-          font-weight: bold;
+}
+@media screen and (max-width:934px){
+    #container p{
+       font-size:13px;
+       height:10px;
     }
     #container{
-      gap:1%;
+      margin-top:90px;
+      gap:40px;
     }
 }
-@media screen and (max-width:769px){
+@media screen and (max-width:831px){
     #container1 p{
-        font-size:10px;
-        line-height:20px;
-    }
-    #container{
-        gap:26px;
+       line-height:21px;
     }
 }
-@media screen and (max-width:796px){
+@media screen and (max-width:880px){
     #kkplogo img{
-      height:35vh;
-      margin-top:16px;
+         height:34vh;
+    }
+    #container1 p{
+           font-size:14px;
+           line-height:29px;
     }
 }
-@media screen and (max-width:539px){
-    #kkplogo img{
-      height:30vh;
+@media screen and (max-width:804px){
+    #container1 p{
+      line-height:20px;
     }
-}
-@media screen and (max-width:465px){
     #container{
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          margin-top:23%;
-    }
-    #container1 p{
-        font-size:12px;
-        width:75%;
-        margin-left:69px;
+         gap:38px;
     }
 }
-@media screen and (max-width:389px){
+@media screen and (max-width:742px){
     #container1 p{
-       margin-left:50px;
+       font-size:13px;
+    }
+    #kkplogo img{
+        height:35vh;
     }
 }
-@media screen and (max-width:312px){
-    #container1 p{
-       margin-left:35px;
+@media screen and (max-width:656px){
+    #kkplogo img{
+     height:33vh;
     }
 }
-@media screen and (min-width:1023px) and (max-width:1025px){
+@media screen and (max-width:607px){
     #container1 p{
-          font-size:12px;
-          margin-top:20px;
+       font-size:10px;
+    }
+    #kkplogo img{
+     height:30vh;
     }
 }
-@media screen and (max-width:426px){
-    #container1 p{
-       margin-left:54px;
-    }
-
+@media screen and (max-width:576px){
+#container{
+    display: flex;
+    flex-direction: column;
 }
+#kkplogo img{
+       height:43vh;
+       width:69vw;
+        margin-left:15%;
+}
+#container1 p{
+     width:70vw;
+     font-size:13px;
+     margin-left:15%;
+     line-height:23px;
+     margin-top:29px;
+}
+}
+@media screen and (max-width:430px){
+    #main{
+        height: 155vh;
+    }
+}
+@media screen and (max-width:392px){
+    #kkplogo img{
+       margin-left:68px;
+    }
+}
+@media screen and (max-width:343px){
+    #kkplogo img{
+       margin-left:60px;
+    }
+    #container1 p{
+         font-size:12.9px;  
+    }
+}
+@media screen and (max-width:322px){
+    #about{
+       margin-left:20px;
+    }
+    #kkplogo img{
+        width:40vw;
+        height: 20vh;
+        margin-left:100px;
+    }
+}
+@media screen and (min-width:1441px)and (max-width:2561px){
+    #container1 p{
+        font-size:30px;
+    }
+}
+@media screen and (min-width:605px) and (max-width:770px){
+#container1 p{
+     line-height:27px;
+     font-size:13px;
+}
+}
+@media screen and (min-width:576px) and (max-width:605px){
+    #container1 p{
+       line-height:25px;
+       font-size:13px;
+    }
+}
+@media screen and (max-width:380px){
+    #main{
+      height:165vh;
+    }
+}
+@media screen and (max-width:325px){
+    #main{
+     height:180vh;
+    }
+}
+@media screen and (max-width:321px){
+    #main{
+      height:155vh;
+    }
+    #about{
+       font-size:50px;
+    }
+}
+@media screen and (min-width:374px) and (max-width:376px){
+    #kkplogo img{
+       height:40vh;
+       margin-left:49px;
+    }
+}
+@media screen and (min-width:767px) and (max-width:780px){
+    #kkplogo img{
+       margin-top:60%;
+    }
+    #main{
+       height:110vh;
+    }
+    #container{
+       gap:9%;
+    }
+    }
+    @media screen and (min-width:374px) and (max-width:376px){
+        #kkplogo img{
+          margin-left:60px;
+        }
+        #about{
+            font-size:50px;
+        }
+    }
+    @media screen and (min-width:1023px) and (max-width:1025px){
+        #kkplogo img{
+            margin-top:130px;
+        }
+        #main{
+          height:120vh;
+        }
+    }
+    @media screen and (min-width:1439px) and (max-width:1441px){
+        #main{
+          height:78vh;
+        }
+    }
+    @media screen and (min-width:2559px) and (max-width:2561px){
+        #main{
+         height:80vh;
+        }
+    }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     import About from "../components/about.svelte";
     import Chairman from "../components/chairman.svelte";
     import Features from "../components/features.svelte";
-</script>
+    </script>
 <section class="">
     <div class="">
         <Navbar/>
